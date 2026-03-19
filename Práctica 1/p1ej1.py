04a4aff79d91fe64f2d8ec4f425545abb6296e88
Aldana Laiño <aldana.working@gmail.com>
2026-03-19 13:33:47 -0300

    edité el ej1

--- Práctica 1/p1ej1.py
+++ Práctica 1/p1ej1.py
@@ -1,6 +1,6 @@
 #Escribe un programa que solicite al usuario su año de nacimiento y muestre en qué año cumplirá 18, 21 y 100 años.
 
 birth_year = int(input('Enter your birth year: '))
-print(f"You are going to have 18 in {birth_year + 18}")
-print(f"You are going to have 21 in {birth_year + 21}")
-print(f"You are going to have 100 in {birth_year + 100}")
+print(f"You are going to be 18 in {birth_year + 18}")
+print(f"You are going to be 21 in {birth_year + 21}")
+print(f"You are going to be 100 in {birth_year + 100}")
