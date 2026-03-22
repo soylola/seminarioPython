a931c71f6a2d521bd9e72ec2a1a8637c688f54c1
Aldana Laiño <aldana.working@gmail.com>
2026-03-22 12:38:23 -0300

    agregué puntaje al ejercicio

--- Práctica 1/questions.py
+++ Práctica 1/questions.py
@@ -15,6 +15,7 @@ words = [
 word = random.choice(words)
 guessed = []
 attempts = 6
+points = 0
 print("¡Bienvenido al Ahorcado!")
 print()
 while attempts > 0:
@@ -28,22 +29,29 @@ while attempts > 0:
     print(progress)
 # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
+        points += 6
         print("¡Ganaste!")
+        print(f"Puntos actuales: {points}")
         break
     print(f"Intentos restantes: {attempts}")
     print(f"Letras usadas: {', '.join(guessed)}")
+    print(f"Puntos: {points}")
 
     letter = input("Ingresá una letra: ")
-
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
+    if len(letter)==1:
+        if letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
+        else:
+            guessed.append(letter)
+            attempts -= 1
+            points -= 1
+            print("Esa letra no está en la palabra.")
+        print()
     else:
-        guessed.append(letter)
-        attempts -= 1
-        print("Esa letra no está en la palabra.")
-    print()
+        print('Entrada no válida.')
 else:
+    points = 0
     print(f"¡Perdiste! La palabra era: {word}")
